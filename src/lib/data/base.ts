2c6a9c88ea674028470ac79643b5d2d539e256ba
Md Saklen <dev.mdsaklen@hotmail.com>
2025-02-13 20:38:31 +0530

    Update base.ts

--- src/lib/data/base.ts
+++ src/lib/data/base.ts
@@ -1,6 +1,6 @@
-const firstName = 'Riadh';
-const lastName = 'Adrani';
-const suffix = 'Slick Portfolio With Svelte 5';
+const firstName = 'Md';
+const lastName = 'Saklen';
+const suffix = '$whois:saklen';
 
 const BaseData = {
 	firstName,
