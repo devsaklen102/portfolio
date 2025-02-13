33ca145fba47df986817360690a44613cfbff90c
Md Saklen <dev.mdsaklen@hotmail.com>
2025-02-13 21:22:27 +0530

    Update resume.ts

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -2,7 +2,7 @@ import { href } from '$lib/utils';
 
 const title = 'Resumé';
 
-const resume = href('/pdf/resume.pdf');
+const resume = href('/resume.pdf');
 
 const ResumeData = { title, resume };
 
