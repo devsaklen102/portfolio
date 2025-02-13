467be62fe709216a07f957bed29d0059e2eee514
Md Saklen <dev.mdsaklen@hotmail.com>
2025-02-13 22:07:36 +0530

    Update nav-bar.ts

--- src/lib/data/nav-bar.ts
+++ src/lib/data/nav-bar.ts
@@ -8,7 +8,7 @@ const items: Array<{
 	href: string;
 }> = [
 	{ title: 'Skills', icon: 'i-carbon-assembly-cluster', href: '/skills' },
-	{ title: 'Projects', icon: 'i-carbon-cube', href: '/projects' },
+	//{ title: 'Projects', icon: 'i-carbon-cube', href: '/projects' },
 	{ title: 'Experience', icon: 'i-carbon-development', href: '/experience' },
 	{ title: 'Education', icon: 'i-carbon-education', href: '/education' },
 	{ title: 'Resume', icon: 'i-carbon-document', href: '/resume' }
