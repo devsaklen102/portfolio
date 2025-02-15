87a59dfc5a50b177edb97eadb79573b308deefee
Md Saklen <dev.mdsaklen@hotmail.com>
2025-02-16 02:32:10 +0530

    Update education-card.svelte

--- src/lib/components/education/education-card.svelte
+++ src/lib/components/education/education-card.svelte
@@ -53,15 +53,7 @@
 				</TooltipTrigger>
 				<TooltipContent>Date range</TooltipContent>
 			</Tooltip>
-			<Tooltip openDelay={300}>
-				<TooltipTrigger>
-					/*<Muted className="flex flex-row items-center gap-2">
-						<Icon icon="i-carbon-time" />
-						<div>{exactDuration}</div>
-					</Muted>*/
-					<TooltipContent side="bottom">Exact duration</TooltipContent>
-				</TooltipTrigger>
-			</Tooltip>
+	
 			<div class="py-2 text-sm text-muted-foreground">{ellipsify(it.shortDescription, 150)}</div>
 			<div class="flex flex-row flex-wrap gap-2">
 				{#each it.subjects as subject (subject)}
